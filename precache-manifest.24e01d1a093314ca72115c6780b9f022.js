35ad820700668c6d6367be8d3a421c69a8604e8d
Lovee93 <lovee.jain93@gmail.com>
2020-03-31 20:18:24 +1100

    Updates

---
+++ precache-manifest.24e01d1a093314ca72115c6780b9f022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4476f21c72ca5e4b39325e418000eb",
+    "url": "/swapi_calypse/index.html"
+  },
+  {
+    "revision": "59d23c9499ebd86cfd85",
+    "url": "/swapi_calypse/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "44168dd76c827cf624b9",
+    "url": "/swapi_calypse/static/js/2.29bcd78c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swapi_calypse/static/js/2.29bcd78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d23c9499ebd86cfd85",
+    "url": "/swapi_calypse/static/js/main.bc27ca44.chunk.js"
+  },
+  {
+    "revision": "f36dc03ded783e0a1821",
+    "url": "/swapi_calypse/static/js/runtime-main.2c2b3f8b.js"
+  }
+]);
