a0458a387e2592128f7937c0f0ff96651eed530d
Lovee93 <lovee.jain93@gmail.com>
2020-03-31 20:15:53 +1100

    Adding my project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.getRandomInt = this.getRandomInt.bind(this);
+    this.state = {
+      random_num: this.getRandomInt(1,50),
+      name: ''
+    }
+  }
+  
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  };
+
+  
+  componentDidMount() {
+    fetch(`https://swapi.co/api/people/${this.state.random_num}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          name: data.name
+        })
+      }
+    )
+  };
+  
+  render() {
+
+    const { random_num, name } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={`https://robohash.org/${random_num}?set=set5`} />
+          {
+            name === '' ? 
+              <h2> Loading a Star Warrior for you... </h2>
+            :
+              <h2>
+                Hey {name}
+              </h2>
+          }
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
